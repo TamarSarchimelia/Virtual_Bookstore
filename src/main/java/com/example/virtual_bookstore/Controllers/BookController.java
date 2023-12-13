29faae1a1a897de1cc468f9abf7d24021613fe3b
TamarSarchimelia <tsarc21@freeuni.edu.ge>
2023-12-13 22:51:09 +0400

    something added in book controller

--- src/main/java/com/example/virtual_bookstore/Controllers/BookController.java
+++ src/main/java/com/example/virtual_bookstore/Controllers/BookController.java
@@ -1,4 +1,11 @@
 package com.example.virtual_bookstore.Controllers;
 
+import com.example.virtual_bookstore.Repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookController {
+
+    private BookRepository bookRepository;
 }
